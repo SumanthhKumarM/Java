027684ad8e78f4aef1138532ac07867810a9e582
SumanthhKumarM <113960373+SumanthhKumarM@users.noreply.github.com>
2023-01-24 19:01:55 +0530

    Update and rename addarray.java to comarray.java

--- comarray.java
+++ comarray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.*;
-class addarray{
+class comarray{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		List<Integer> l=new ArrayList<Integer>();
