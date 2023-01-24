7ecd9f56161dfe1cf1261bbb09c851f67f20d5f5
SumanthhKumarM <113960373+SumanthhKumarM@users.noreply.github.com>
2023-01-24 19:07:21 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Test {
+private int value = 10;
+void method() {
+int value = 40;
+System.out.println("Value of Instance variable : "+ this.value);
+System.out.println("Value of Local variable : "+ value);
+}
+}
+ 
+class GFG {
+public static void main(String args[]) {
+Test obj1 = new Test();
+obj1.method();
+}
+}
